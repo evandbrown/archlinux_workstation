05d33221b1664e35b6f5b6e25e5d9b8e32489b45
Evan Brown <evanbrown@google.com>
2015-05-16 20:37:13 -0700

    Synaptics config gui

--- manifests/lenovox1gen3.pp
+++ manifests/lenovox1gen3.pp
@@ -7,6 +7,7 @@
 class archlinux_workstation::lenovox1gen3 {
   package {'xf86-video-intel': ensure => present, }
   package {'xf86-input-synaptics': ensure => present, }
+  package {'gpointing-device-settings': ensure => present,}
   package {'tlp': ensure => present, }
   package {'network-manager-applet': ensure => present, }
 
